825aec98d00d9d91b1ec8e5885a5146d4e6785bc
shimrity <shimrit.moryossef@gmail.com>
2020-07-22 15:34:13 +0300

    color picker

--- src/main.js
+++ src/main.js
@@ -6,41 +6,27 @@ import { useState } from 'react';
 import '../css/main.css';
 
 const App = () => {
-  const [sec, setSecVal] = useState(0);
-  const [minit, setMiniVal] = useState(0);
-  const [hour, setHourVal] = useState(0);
- 
-  function handleSecondChange(e)
-  {
-    const val= Number(e.target.value)
-    setSecVal(val);
-    setMiniVal(x => val/60);
-    setHourVal(x => val/3600);
-   
-  }
-  function handleMinitsChange(e)
-  {
-   
-    const val= Number(e.target.value)
-    setMiniVal(val);
-    setSecVal(x => val*60);
-    setHourVal(x => val/60);
+  const [backgroud, setBackgroudVal] = useState("#000000");
+  
 
-  }
-  function handleHoursChange(e)
+  function handleColorChange(e)
   {
-    const val= Number(e.target.value)
-    setHourVal(val);
-    setMiniVal(x => val*60); 
-    setSecVal(x => val*3600);
     
+    setBackgroudVal(e.target.value);
   }
+ 
+  
   return (
     <div>
       <h1>Enter time:</h1>
-     <p>seconds: <input type="number" value={sec} onChange={handleSecondChange}></input> </p>
-     <p>minits: <input type="number" value={minit} onChange={handleMinitsChange}></input> </p>
-     <p>hours: <input type="number" value={hour} onChange={handleHoursChange}></input> </p>      
+     <p>pick colore: <input type="color" onChange={handleColorChange}></input> </p>
+      <div style={{
+        background: backgroud,
+        fontSize: "40px",
+        width:"100px",
+        }}>
+            color
+      </div>
      
     </div>
   )
