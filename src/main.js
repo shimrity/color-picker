6fcacabcf7f5bb27748babbe6adbea39b2a47a7e
shimrity <shimrit.moryossef@gmail.com>
2020-07-22 11:26:05 +0300

    time units in 1 commponent

--- src/main.js
+++ src/main.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Text from './Text';
+
 import { useState } from 'react';
 
 import '../css/main.css';
 
 const App = () => {
-  const [valu, setVale] = useState(0);
-  function inc(val,f)
+  const [sec, setSecVal] = useState(0);
+  const [minit, setMiniVal] = useState(0);
+  const [hour, setHourVal] = useState(0);
+ 
+  function handleSecondChange(e)
   {
-    //alert(val);
-    valu =  val/f;
-    setVale(valu);
+    const val= Number(e.target.value)
+    setSecVal(val);
+    setMiniVal(x => val/60);
+    setHourVal(x => val/3600);
+   
   }
-  
+  function handleMinitsChange(e)
+  {
+   
+    const val= Number(e.target.value)
+    setMiniVal(val);
+    setSecVal(x => val*60);
+    setHourVal(x => val/60);
+
+  }
+  function handleHoursChange(e)
+  {
+    const val= Number(e.target.value)
+    setHourVal(val);
+    setMiniVal(x => val*60); 
+    setSecVal(x => val*3600);
+    
+  }
   return (
     <div>
       <h1>Enter time:</h1>
-     <Text val={valu} inc={inc} factor={1}/> <br/>
-     <Text val={valu} inc={inc} factor={60}/> <br/>
-     <Text val={valu} inc={inc} factor={3600}/> <br/>    
+     <p>seconds: <input type="number" value={sec} onChange={handleSecondChange}></input> </p>
+     <p>minits: <input type="number" value={minit} onChange={handleMinitsChange}></input> </p>
+     <p>hours: <input type="number" value={hour} onChange={handleHoursChange}></input> </p>      
      
     </div>
   )
